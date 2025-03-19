b9feec52aff5dca8b35334de7336969c0849658e
naoasa <asaumi1106n@gmail.com>
2025-03-20 06:45:43 +0900

    feat: ゲストログイン #16

--- easy_readzy/config/routes.rb
+++ easy_readzy/config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { sessions: "users/sessions" }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # ゲストログイン用のルーティング
+  devise_scope :user do
+    post "users/guest_sign_in", to: "users/sessions#guest_sign_in"
+  end
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
