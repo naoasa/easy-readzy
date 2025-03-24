addd5bf969ddf162e7cb83e1745ed692ab966875
naoasa <asaumi1106n@gmail.com>
2025-03-24 23:18:43 +0900

    fix: 本棚と本が中間テーブルを介するようにアソシエーションを修正 #18

--- easy_readzy/app/models/bookshelf.rb
+++ easy_readzy/app/models/bookshelf.rb
@@ -2,6 +2,7 @@ class Bookshelf < ApplicationRecord
   # アソシエーション
   belongs_to :user
   has_many :bookshelf_books, dependent: :destroy # 本棚が消えたら、中間テーブルも消える
+  has_many :books, through: :bookshelf_books # 本棚は中間テーブルを媒介に本を持つ
 
   # バリデーション
   validates :user_id, presence: true
