f44f98098c591982b28f152b3ba09932e484b265
naoasa <asaumi1106n@gmail.com>
2025-03-31 21:53:03 +0900

    feat: ログイン後はユーザーの本棚の本一覧に遷移する #18

--- easy_readzy/app/controllers/application_controller.rb
+++ easy_readzy/app/controllers/application_controller.rb
@@ -1,4 +1,18 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  # Deviseのafter_sign_in_path_forメソッドをオーバーライド
+  def after_sign_in_path_for(resource)
+    # ログインユーザーの最小の本棚IDを取得
+    first_bookshelf_id = resource.bookshelves.minimum(:id)
+
+    # 本棚が存在する場合は、その本棚の本一覧ページにリダイレクト
+    if first_bookshelf_id
+      user_bookshelf_books_path(user_id: resource.id, bookshelf_id: first_bookshelf_id)
+    else
+      # 本棚が存在しない場合はルートURLにリダイレクト
+      root_path
+    end
+  end
 end
