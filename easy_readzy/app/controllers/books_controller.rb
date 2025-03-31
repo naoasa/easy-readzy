d66e18b4ac3086189c6921fcb63e5e7a2ecfbe9a
naoasa <asaumi1106n@gmail.com>
2025-03-31 12:51:56 +0900

    feat: ルーティング"/users/:id/bookshelves/:id/books"に対応 #18

--- easy_readzy/app/controllers/books_controller.rb
+++ easy_readzy/app/controllers/books_controller.rb
@@ -1,4 +1,12 @@
 class BooksController < ApplicationController
   def index
+    # ユーザーを取得
+    @user = User.find(params[:user_id])
+
+    # 本棚を取得
+    @bookshelf = @user.bookshelves.find(params[:bookshelf_id])
+
+    # 本棚の本一覧を取得
+    @books = @bookshelf.books
   end
 end
