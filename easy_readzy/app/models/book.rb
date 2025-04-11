71c66b0208239f651123c5ffde0070e137437148
naoasa <asaumi1106n@gmail.com>
2025-04-11 23:09:36 +0900

    chore: 不要になったバリデーションを削除 #26

--- easy_readzy/app/models/book.rb
+++ easy_readzy/app/models/book.rb
@@ -5,7 +5,6 @@ class Book < ApplicationRecord
   # バリデーション
   validates :google_book_id, presence: true, uniqueness: true, length: { maximum: 100 }
   validates :title, presence: true, length: { maximum: 500 }
-  validates :cover_image_url, length: { maximum: 2500 }
   validates :author, length: { maximum: 100 }
   validates :publisher, length: { maximum: 100 }
   validates :published_date, length: { maximum: 100 }
