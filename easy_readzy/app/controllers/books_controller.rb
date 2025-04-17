896f5541edca42160532186e7f10f32fa0a3444f
naoasa <asaumi1106n@gmail.com>
2025-04-17 12:53:21 +0900

    feat: クエリをもとにAPIを叩いて検索結果を得る #28

--- easy_readzy/app/controllers/books_controller.rb
+++ easy_readzy/app/controllers/books_controller.rb
@@ -1,6 +1,18 @@
 class BooksController < ApplicationController
+  require "google/apis/books_v1"
+
   # Deviseのヘルパーメソッドでログインユーザーのみがコントローラーのアクションを実行できるようにする
   before_action :authenticate_user!
+
+  def search
+    @query = params[:query]
+    books = Google::Apis::BooksV1::BooksService.new
+    books.key = ENV["GOOGLE_BOOKS_API_KEY"]
+
+    @results = books.list_volumes(@query, max_results: 4)
+    @books = @results.items || []
+  end
+
   def index
     # ユーザーを取得
     @user = User.find(params[:user_id])
