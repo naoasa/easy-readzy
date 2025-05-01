467a521c9b8737665581147b71bf0551f3b58411
naoasa <asaumi1106n@gmail.com>
2025-05-01 20:16:07 +0900

    chore: jsファイル名を変更 #34

--- easy_readzy/app/javascript/application.js
+++ easy_readzy/app/javascript/application.js
@@ -1,4 +1,4 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 
 import './pages/header';
-import './pages/createGoal';
+import './pages/saveToBookshelf';
