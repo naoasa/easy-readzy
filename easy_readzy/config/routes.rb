f7bd0be8438f1d27da9b57fe5dc169566b8b3cf1
naoasa <asaumi1106n@gmail.com>
2025-05-05 21:59:53 +0900

    feat: indexページの書影クリックでshowページに遷移 #36

--- easy_readzy/config/routes.rb
+++ easy_readzy/config/routes.rb
@@ -15,7 +15,7 @@ Rails.application.routes.draw do
   # ユーザーの本棚の本一覧ページのルーティング
   resources :users do
     resources :bookshelves do
-      resources :books, only: [ :index, :new, :create ]
+      resources :books, only: [ :index, :new, :create, :show ]
     end
   end
 
