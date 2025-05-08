68d5859aaeed7e86a89159a16ce055ebe25d533f
naoasa <asaumi1106n@gmail.com>
2025-05-08 18:58:33 +0900

    feat: アウトプット作成のルーティング #36

--- easy_readzy/config/routes.rb
+++ easy_readzy/config/routes.rb
@@ -26,6 +26,11 @@ Rails.application.routes.draw do
     end
   end
 
+  # アウトプット作成のためのルーティング
+  resources :goals, only: [] do
+    resource :output, only: [ :create ]
+  end
+
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
