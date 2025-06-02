c5e4daa7f8b5d01ddb73280ff172d60cdf2ada40
naoasa <asaumi1106n@gmail.com>
2025-06-03 00:53:31 +0900

    feat: バツをクリックでflashを非表示 #38

--- easy_readzy/app/javascript/pages/flash.js
+++ easy_readzy/app/javascript/pages/flash.js
@@ -1,8 +1,18 @@
 document.addEventListener('DOMContentLoaded', () => {
   const flash = document.querySelector('.flash');
+  const closeFlash = document.getElementById('close_flash');
+
   if (flash) {
+    // 5秒後にフェードアウト
     setTimeout(() => {
       flash.classList.add('fade-out');
     }, 5000); // 5秒後にfade-outクラスを追加
+
+    // バツ印をクリックで即時フェードアウト
+    if (closeFlash) {
+      closeFlash.addEventListener('click', () => {
+        flash.classList.add('fade-out');
+      });
+    }
   }
 });
