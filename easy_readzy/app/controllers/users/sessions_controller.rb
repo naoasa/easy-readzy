4dfe96206d3e033e0010863483eeba76fba696d1
naoasa <asaumi1106n@gmail.com>
2025-06-03 01:20:31 +0900

    chore: flash文言の変更 #38

--- easy_readzy/app/controllers/users/sessions_controller.rb
+++ easy_readzy/app/controllers/users/sessions_controller.rb
@@ -6,7 +6,7 @@ class Users::SessionsController < Devise::SessionsController
   def guest_sign_in
     user = User.guest
     sign_in user
-    redirect_to after_sign_in_path_for(user), notice: "ゲストユーザーとしてログインしました。"
+    redirect_to after_sign_in_path_for(user), notice: "ゲストユーザーとしてログインしました"
   end
 
   # GET /resource/sign_in
