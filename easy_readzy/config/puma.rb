e61e3ebb8f40b43434881db31b12ca261053ec27
naoasa <asaumi1106n@gmail.com>
2025-08-03 01:30:50 +0900

    chore: Puma設定ファイルの変更

--- easy_readzy/config/puma.rb
+++ easy_readzy/config/puma.rb
@@ -7,10 +7,10 @@ pidfile "#{app_root}/tmp/pids/puma.pid"
 state_path "#{app_root}/tmp/pids/puma.state"
 stdout_redirect "#{app_root}/log/puma.stdout.log", "#{app_root}/log/puma.stderr.log", true
 
-threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
+threads_count = ENV.fetch("RAILS_MAX_THREADS", 3)
 threads threads_count, threads_count
-environment ENV.fetch("RAILS_ENV") { "production" }
 
+environment ENV.fetch("RAILS_ENV") { "production" }
 workers ENV.fetch("WEB_CONCURRENCY") { 2 }
 preload_app!
 
