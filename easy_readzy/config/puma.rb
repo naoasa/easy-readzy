9bfa276ceb198fd3705e49c7f577d995828d021c
naoasa <asaumi1106n@gmail.com>
2025-08-03 02:00:49 +0900

    chore: change puma.rb

--- easy_readzy/config/puma.rb
+++ easy_readzy/config/puma.rb
@@ -1,17 +1,17 @@
-# Puma の設定ファイル
+# config/puma.rb
 
-app_root = File.expand_path("../..", __FILE__)
-bind "unix://#{app_root}/tmp/sockets/puma.sock"
+app_dir = File.expand_path("../..", __FILE__)
+shared_dir = "#{app_dir}/tmp"
 
-pidfile "#{app_root}/tmp/pids/puma.pid"
-state_path "#{app_root}/tmp/pids/puma.state"
-stdout_redirect "#{app_root}/log/puma.stdout.log", "#{app_root}/log/puma.stderr.log", true
+bind "unix://#{shared_dir}/sockets/puma.sock"
+pidfile "#{shared_dir}/pids/puma.pid"
+state_path "#{shared_dir}/pids/puma.state"
+stdout_redirect "#{app_dir}/log/puma.stdout.log", "#{app_dir}/log/puma.stderr.log", true
 
-threads_count = ENV.fetch("RAILS_MAX_THREADS", 3)
+threads_count = ENV.fetch("RAILS_MAX_THREADS", 3).to_i
 threads threads_count, threads_count
+workers ENV.fetch("WEB_CONCURRENCY", 2)
 
-environment ENV.fetch("RAILS_ENV") { "production" }
-workers ENV.fetch("WEB_CONCURRENCY") { 2 }
 preload_app!
 
-plugin :tmp_restart
+environment ENV.fetch("RAILS_ENV") { "production" }
