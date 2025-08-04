26d8fc1343e6004618e58ffd3681ec27697a335f
naoasa <asaumi1106n@gmail.com>
2025-08-04 21:46:55 +0900

    chore: 初期データ

--- easy_readzy/db/seeds.rb
+++ easy_readzy/db/seeds.rb
@@ -1,9 +1,5 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+# 初期ユーザーを作成
+guest = User.find_or_initialize_by(email: "guest@example.com")
+guest.name = "guest_user"
+guest.password = SecureRandom.hex(10)
+guest.save!
